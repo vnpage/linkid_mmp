5b3a4a0dc369cc6963cb91a8a63510877c8853cc
Tuan Dinh <leonacky@gmail.com>
2022-12-30 11:02:26 +0700

    init
    Signed-off-by: Tuan Dinh <leonacky@gmail.com>

---
+++ Example/Pods/Target Support Files/Pods-linkid_mmp_Tests/Pods-linkid_mmp_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_linkid_mmp_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_linkid_mmp_Tests
+@end
---
+++ Example/Pods/Target Support Files/linkid_mmp/linkid_mmp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_linkid_mmp : NSObject
+@end
+@implementation PodsDummy_linkid_mmp
+@end
